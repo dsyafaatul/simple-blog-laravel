bed5db050abee97ec57a634749ff96828a1f976a
D. Syafa'atul Anbiya <dikisyafaatul@gmail.com>
2023-09-13 22:45:25 +0700

    Post Category & Eloquent Relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,4 +15,8 @@ class Post extends Model
     //     'body'
     // ];
     protected $guarded = ['id'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
