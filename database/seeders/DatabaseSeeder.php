326636efb946ddfecee568932438f91d5176287f
D. Syafa'atul Anbiya <dikisyafaatul@gmail.com>
2023-09-23 07:17:22 +0700

    Factory & Faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,24 +15,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        User::create([
-            'name' => "D. Syafa'atul Anbiya",
-            'email' => 'dikisyafaatul@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => "D. Syafa'atul Anbiya",
+        //     'email' => 'dikisyafaatul@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
 
-        User::create([
-            'name' => "D. Jama'atul Anbiya",
-            'email' => 'djamaatul.anbiya@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => "D. Jama'atul Anbiya",
+        //     'email' => 'djamaatul.anbiya@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -44,40 +44,42 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Post Pertama',
-            'slug' => 'post-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut?',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut? Excepturi nesciunt dicta perferendis ipsam neque, dolor unde laudantium repellendus illum nam error pariatur voluptatibus, architecto veniam harum consequuntur incidunt! Eum neque dolor laudantium quasi magnam? Nihil iusto nostrum beatae deserunt id, eius at autem aperiam ut laboriosam culpa minus atque provident, debitis veniam delectus ipsam. Illum laborum magni ipsam in blanditiis sunt molestiae. Ad, distinctio consectetur repellat nulla ullam perspiciatis exercitationem, fuga quo possimus, cumque illo itaque accusamus modi. Ab blanditiis autem similique odio?',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Post Kedua',
-            'slug' => 'post-kedua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut?',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut? Excepturi nesciunt dicta perferendis ipsam neque, dolor unde laudantium repellendus illum nam error pariatur voluptatibus, architecto veniam harum consequuntur incidunt! Eum neque dolor laudantium quasi magnam? Nihil iusto nostrum beatae deserunt id, eius at autem aperiam ut laboriosam culpa minus atque provident, debitis veniam delectus ipsam. Illum laborum magni ipsam in blanditiis sunt molestiae. Ad, distinctio consectetur repellat nulla ullam perspiciatis exercitationem, fuga quo possimus, cumque illo itaque accusamus modi. Ab blanditiis autem similique odio?',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Post Pertama',
+        //     'slug' => 'post-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut?',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut? Excepturi nesciunt dicta perferendis ipsam neque, dolor unde laudantium repellendus illum nam error pariatur voluptatibus, architecto veniam harum consequuntur incidunt! Eum neque dolor laudantium quasi magnam? Nihil iusto nostrum beatae deserunt id, eius at autem aperiam ut laboriosam culpa minus atque provident, debitis veniam delectus ipsam. Illum laborum magni ipsam in blanditiis sunt molestiae. Ad, distinctio consectetur repellat nulla ullam perspiciatis exercitationem, fuga quo possimus, cumque illo itaque accusamus modi. Ab blanditiis autem similique odio?',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Post Ketiga',
-            'slug' => 'post-ketiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut?',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut? Excepturi nesciunt dicta perferendis ipsam neque, dolor unde laudantium repellendus illum nam error pariatur voluptatibus, architecto veniam harum consequuntur incidunt! Eum neque dolor laudantium quasi magnam? Nihil iusto nostrum beatae deserunt id, eius at autem aperiam ut laboriosam culpa minus atque provident, debitis veniam delectus ipsam. Illum laborum magni ipsam in blanditiis sunt molestiae. Ad, distinctio consectetur repellat nulla ullam perspiciatis exercitationem, fuga quo possimus, cumque illo itaque accusamus modi. Ab blanditiis autem similique odio?',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Post Kedua',
+        //     'slug' => 'post-kedua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut?',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut? Excepturi nesciunt dicta perferendis ipsam neque, dolor unde laudantium repellendus illum nam error pariatur voluptatibus, architecto veniam harum consequuntur incidunt! Eum neque dolor laudantium quasi magnam? Nihil iusto nostrum beatae deserunt id, eius at autem aperiam ut laboriosam culpa minus atque provident, debitis veniam delectus ipsam. Illum laborum magni ipsam in blanditiis sunt molestiae. Ad, distinctio consectetur repellat nulla ullam perspiciatis exercitationem, fuga quo possimus, cumque illo itaque accusamus modi. Ab blanditiis autem similique odio?',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Post Keempat',
-            'slug' => 'post-keempat',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut?',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut? Excepturi nesciunt dicta perferendis ipsam neque, dolor unde laudantium repellendus illum nam error pariatur voluptatibus, architecto veniam harum consequuntur incidunt! Eum neque dolor laudantium quasi magnam? Nihil iusto nostrum beatae deserunt id, eius at autem aperiam ut laboriosam culpa minus atque provident, debitis veniam delectus ipsam. Illum laborum magni ipsam in blanditiis sunt molestiae. Ad, distinctio consectetur repellat nulla ullam perspiciatis exercitationem, fuga quo possimus, cumque illo itaque accusamus modi. Ab blanditiis autem similique odio?',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Post Ketiga',
+        //     'slug' => 'post-ketiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut?',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut? Excepturi nesciunt dicta perferendis ipsam neque, dolor unde laudantium repellendus illum nam error pariatur voluptatibus, architecto veniam harum consequuntur incidunt! Eum neque dolor laudantium quasi magnam? Nihil iusto nostrum beatae deserunt id, eius at autem aperiam ut laboriosam culpa minus atque provident, debitis veniam delectus ipsam. Illum laborum magni ipsam in blanditiis sunt molestiae. Ad, distinctio consectetur repellat nulla ullam perspiciatis exercitationem, fuga quo possimus, cumque illo itaque accusamus modi. Ab blanditiis autem similique odio?',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Post Keempat',
+        //     'slug' => 'post-keempat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut?',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eum, libero repellendus, qui, dicta vitae laboriosam quos hic illo explicabo rem iste similique soluta aut? Excepturi nesciunt dicta perferendis ipsam neque, dolor unde laudantium repellendus illum nam error pariatur voluptatibus, architecto veniam harum consequuntur incidunt! Eum neque dolor laudantium quasi magnam? Nihil iusto nostrum beatae deserunt id, eius at autem aperiam ut laboriosam culpa minus atque provident, debitis veniam delectus ipsam. Illum laborum magni ipsam in blanditiis sunt molestiae. Ad, distinctio consectetur repellat nulla ullam perspiciatis exercitationem, fuga quo possimus, cumque illo itaque accusamus modi. Ab blanditiis autem similique odio?',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
     }
 }
